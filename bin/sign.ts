4f9ccd9008fc71be434b65cf57d5b71d4d20dc5d
Eric Prud'hommeaux <eric+github@w3.org>
2021-03-19 00:50:45 +0100

    + verification

--- bin/sign.ts
+++ bin/sign.ts
@@ -1,18 +1,20 @@
 /**
- * example invocation:
- sign examples/pubKey.json examples/privKey.json examples/medicationrequest0301-lite.json yourMom > medreq-signed.json
+ * (eventual) example invocation:
+ sign examples/privKey.json examples/medicationrequest0301-lite.json yourMom > medreq-signed.json
+ check examples/pubKey.json medreq-signed.json
  */
 
 const Fs = require('fs');
 import { FHIRSign } from '../src/FHIRsign';
 
-const [pub, priv, input, issuer] = process.argv.slice(2);
+const [pub, priv, resourceFile, issuer] = process.argv.slice(2);
 const s = new FHIRSign(readJson(pub), readJson(priv));
-const signed = s.sign(readJson(input), issuer);
+const signed = s.sign(readJson(resourceFile), issuer).replace(/\.[^.]+\./, '..');
 console.log('signed (still a JWS, should be patched resource):', signed);
 
 // Check it now, but this code will go in bin/check.ts.
-const checked = s.check(signed);
+const checkMe = JSON.parse(JSON.stringify(resourceFile)); // copy it
+const checked = s.check(readJson(resourceFile), signed);
 console.log(JSON.stringify(checked, null, 2));
 
 function readJson(filePath: string) {
--- src/FHIRsign.ts
+++ src/FHIRsign.ts
@@ -5,110 +5,136 @@ const canonicalize = require('canonicalize');
 interface ProtectedHeaders { [name: string]: string }
 
 export class FHIRSign {
-    private iz: Array<number> = [1, 2];
-    private pubKey = new chilkat.PublicKey();
-    private privKey = new chilkat.PrivateKey();
+  private iz: Array<number> = [1, 2];
+  private pubKey = new chilkat.PublicKey();
+  private privKey = new chilkat.PrivateKey();
 
-    // Pretend to parameterize this (but leave explicit code for debuggability).
-    private static Algorithms = {
-        "ES256": {
-            hash: {
-                HashAlgorithm: "sha256",
-                Charset: "utf-8",
-            },
-            encode: "encJwsEcdsa",
-            decode: "decJwsEcdsa",
-        }
+  // Pretend to parameterize this (but leave explicit code for debuggability).
+  private static Algorithms = {
+    "ES256": {
+      hash: {
+        HashAlgorithm: "sha256",
+        Charset: "utf-8",
+      },
+      encode: "encodeJwsEcdsa",
+      decode: "decodeJwsEcdsa",
+    }
+  };
+
+  /** Construct a FHIRsign with the pub and priv key as JSON
+   * objects. (See ../examples/privKey.json for an example.)
+   */
+  constructor(pubKeyParm: object, privKeyParm: object) {
+    if (this.pubKey.LoadFromString(JSON.stringify(pubKeyParm)) !== true)
+      throw Error(`public key error: ${this.pubKey.LastErrorText}`);
+
+    if (this.privKey.LoadJwk(JSON.stringify(privKeyParm)) !== true)
+      throw Error(`private key error: ${this.privKey.LastErrorText}`);
+  }
+
+  /** Sign a FHIR Resource.
+   *
+   * TODO: return a resource with the signature injected in the
+   * meta.
+   */
+  sign(resource: object, issuer: string) {
+
+    // Hash resource.
+    const hashString = this.hash(resource, "sha256");
+
+    // JWS protected headers:
+    const headers = { "alg": "ES256", "issuer": issuer };
+    const sig = this.encodeJwsEcdsa(hashString, headers);
+
+    return sig; // !! Should be signed resource.
+  }
+
+  /** Check a FHIR Resource signature.
+   *
+   * TODO: return orig resource
+   */
+  check(resource: object, sig: string): object {
+    const validity = this.decodeJwsEcdsa(sig);
+    const hashString = this.hash(resource, "sha256");
+    console.log(validity.content);
+    console.log(hashString);
+    validity.hashValidity = validity.content === hashString;
+    return validity;
+  }
+
+  hash(resource: object, algorithm: string) {
+    const payload = canonicalize(resource);
+    const crypt = new chilkat.Crypt2();
+    crypt.HashAlgorithm = algorithm;
+    crypt.Charset = "utf-8";
+    const hashBytes = crypt.HashString(payload);
+
+    // Hex-encode bytes with an arbitrary prefix string to give warm fuzzies.
+    const sb = new chilkat.StringBuilder();
+    sb.AppendEncoded(hashBytes, "hex");
+    return "SHA256:" + sb.GetAsString()
+  }
+
+  encodeJwsEcdsa(payloadStr: string, headers: ProtectedHeaders = {}) {
+    // Create the JWS Protected Header
+    var jwsProtHdr = new chilkat.JsonObject();
+    Object.keys(headers).forEach(
+      key => jwsProtHdr.AppendString(key, headers[key])
+    );
+
+    var jws = new chilkat.Jws();
+
+    var signatureIndex = 0; debugger
+    jws.SetProtectedHeader(signatureIndex, jwsProtHdr);
+    jws.SetPrivateKey(signatureIndex, this.privKey);
+    jws.SetPayload(payloadStr, "utf-8", false /* don't include BOM */);
+
+    // I don't see a CreateDetachedJws. Should I be able to strip
+    // out the text between the '..'s and reproduce it given the
+    // signed entity and the public key?
+
+    var jwsCompact = jws.CreateJws(); // default to compact serialization
+    if (jws.LastMethodSuccess !== true)
+      throw Error(`CreateJws error: ${jws.LastErrorText}`);
+    return jwsCompact;
+  }
+
+  decodeJwsEcdsa(last: string) {
+    var jws = new chilkat.Jws();
+
+    // Set the ECC public key:
+    var signatureIndex = 0;
+    jws.SetPublicKey(signatureIndex, this.pubKey);
+
+    // Load the JWS.
+    var sbJws = new chilkat.StringBuilder();
+    sbJws.Append(last)
+
+    let success = jws.LoadJwsSb(sbJws);
+    if (success !== true)
+      throw Error(`LoadJwsSb error: ${jws.LastErrorText}`);
+
+    // Validate the 1st (and only) signature at index 0..
+    var v = jws.Validate(signatureIndex);
+    if (v < 0)
+      throw Error(`Validate error: ${jws.LastErrorText}`);
+
+    if (v == 0)
+      throw Error("Invalid signature.  The ECC key was incorrect, the JWS was invalid, or both.");
+
+    // Examine the protected header:
+    // joseHeader: JsonObject
+    var joseHeader = jws.GetProtectedHeader(signatureIndex);
+    if (jws.LastMethodSuccess !== true)
+      throw Error("No signature header found.");
+
+    joseHeader.EmitCompact = false;
+    return {
+      sigValidity: true,
+      hashValidity: false, // not yet tested
+      content: jws.GetPayload("utf-8"),
+      header: JSON.parse(joseHeader.Emit()),
     };
-
-    constructor(pubKeyParm: any, privKeyParm: any) {
-        if (this.pubKey.LoadFromString(JSON.stringify(pubKeyParm)) !== true)
-            throw Error(`public key error: ${this.pubKey.LastErrorText}`);
-
-        if (this.privKey.LoadJwk(JSON.stringify(privKeyParm)) !== true)
-            throw Error(`private key error: ${this.privKey.LastErrorText}`);
-    }
-
-    sign(resource: object, issuer: string) {
-
-        // Hash resource.
-        const payload = canonicalize(resource);
-        const crypt = new chilkat.Crypt2();
-        crypt.HashAlgorithm = "sha256";
-        crypt.Charset = "utf-8";
-        const hashBytes = crypt.HashString(payload);
-
-        // Hex-encode bytes with an arbitrary prefix string to give warm fuzzies.
-        const sb = new chilkat.StringBuilder();
-        sb.AppendEncoded(hashBytes, "hex");
-        const hashString = "SHA256:" + sb.GetAsString()
-
-        // JWS protected headers:
-        const headers = { "alg": "ES256", "issuer": issuer };
-        const sig = this.encJwsEcdsa(hashString, headers);
-
-        return sig; // !! Should be signed resource.
-    }
-
-    check(sig: string): object {
-        return this.decJwsEcdsa(sig); // !! should be orig resource.
-    }
-
-
-    encJwsEcdsa(payloadStr: string, headers: ProtectedHeaders = {}) {
-        // Create the JWS Protected Header
-        var jwsProtHdr = new chilkat.JsonObject();
-        Object.keys(headers).forEach(
-            key => jwsProtHdr.AppendString(key, headers[key])
-        );
-
-        var jws = new chilkat.Jws();
-
-        var signatureIndex = 0; debugger
-        jws.SetProtectedHeader(signatureIndex, jwsProtHdr);
-        jws.SetPrivateKey(signatureIndex, this.privKey);
-        jws.SetPayload(payloadStr, "utf-8", false /* don't include BOM */);
-        var jwsCompact = jws.CreateJws(); // default to compact serialization
-        if (jws.LastMethodSuccess !== true)
-            throw Error(`CreateJws error: ${jws.LastErrorText}`);
-        return jwsCompact;
-    }
-
-    decJwsEcdsa(last: string) {
-        var jws = new chilkat.Jws();
-
-        // Set the ECC public key:
-        var signatureIndex = 0;
-        jws.SetPublicKey(signatureIndex, this.pubKey);
-
-        // Load the JWS.
-        var sbJws = new chilkat.StringBuilder();
-        sbJws.Append(last)
-
-        let success = jws.LoadJwsSb(sbJws);
-        if (success !== true)
-            throw Error(`LoadJwsSb error: ${jws.LastErrorText}`);
-
-        // Validate the 1st (and only) signature at index 0..
-        var v = jws.Validate(signatureIndex);
-        if (v < 0)
-            throw Error(`Validate error: ${jws.LastErrorText}`);
-
-        if (v == 0)
-            throw Error("Invalid signature.  The ECC key was incorrect, the JWS was invalid, or both.");
-
-        // Examine the protected header:
-        // joseHeader: JsonObject
-        var joseHeader = jws.GetProtectedHeader(signatureIndex);
-        if (jws.LastMethodSuccess !== true)
-            throw Error("No signature header found.");
-
-        joseHeader.EmitCompact = false;
-        return {
-            valid: true,
-            content: jws.GetPayload("utf-8"),
-            header: JSON.parse(joseHeader.Emit()),
-        };
-    }
+  }
 }
 
